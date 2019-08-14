80355442521b774bf650ac24bdb3bc0710855e7d
Stewart Dulaney <stewart.dulaney@tumblr.com>
2019-08-13 20:07:08 -0400

    Add solution for Longest Common Prefix

--- LeetCode/014 - longestCommonPrefix/014 - longestCommonPrefix/main.cpp
+++ LeetCode/014 - longestCommonPrefix/014 - longestCommonPrefix/main.cpp
@@ -7,9 +7,40 @@
 //
 
 #include <iostream>
+#include <vector>
+#include <cassert>
+
+using namespace std;
+
+/*
+ Let n be the number of strings in the array of strings.
+ Let m be the maximum number of characters in any string in the array.
+ Time Complexity: O(m * n)
+ Space Complexity: O(1)
+ */
+
+string longestCommonPrefix(vector<string>& strs) {
+    string result = "";
+    if (strs.size() == 0) {
+        return result;
+    }
+    for (int i = 0; i < strs[0].length(); i++) {
+        for (int j = 1; j < strs.size(); j++) {
+            if (i >= strs[j].length() || strs[j][i] != strs[0][i]) {
+                return result;
+            }
+        }
+        result += strs[0][i];
+    }
+    return result;
+}
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    vector<string> input1 = {"flower", "flow", "flight"};
+    assert(longestCommonPrefix(input1) == "fl");
+    vector<string> input2 = {"dog","racecar","car"};
+    assert(longestCommonPrefix(input2) == "");
+    
     return 0;
 }
